7f93604ce3fd16cb71be2907ebd0d6dd793e0696
xsaz0826 <xsaz0826@gmail.com>
2026-02-25 11:32:06 +0900

    [추가] 회원관리 페이지 리스트, 상세 보기, 수정 부분 비즈니스 로직 구현(Service)

--- src/main/java/com/project/service/MemberService.java
+++ src/main/java/com/project/service/MemberService.java
@@ -1,9 +1,17 @@
 package com.project.service;
 
+import java.util.List;
+
 import com.project.domain.Member;
 
 public interface MemberService {
 
 	public int register(Member member) throws Exception;
+
+	public List<Member> list() throws Exception;
+
+	public Member read(Member member) throws Exception;
+
+	public int modify(Member member) throws Exception;
 
 }
--- src/main/java/com/project/service/MemberServiceImpl.java
+++ src/main/java/com/project/service/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package com.project.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,21 +12,55 @@ import com.project.mapper.MemberMapper;
 
 @Service
 public class MemberServiceImpl implements MemberService {
-	@Autowired 
+	@Autowired
 	private MemberMapper mapper;
 
 	@Transactional
 	@Override
 	public int register(Member member) throws Exception {
 		int count = mapper.register(member);
-		
-		if(count != 0) {
-			// 회원 권한 생성 
-			MemberAuth memberAuth = new MemberAuth(); 
-			memberAuth.setAuth("ROLE_MEMBER"); 
-			
+
+		if (count != 0) {
+			// 회원 권한 생성
+			MemberAuth memberAuth = new MemberAuth();
+			memberAuth.setAuth("ROLE_MEMBER");
+
 			mapper.createAuth(memberAuth);
 		}
 		return count;
-	} 
+	}
+
+	@Override
+	public List<Member> list() throws Exception {
+		return mapper.list();
+	}
+
+	@Override
+	public Member read(Member member) throws Exception {
+		return mapper.read(member);
+	}
+
+	@Transactional
+	@Override
+	public int modify(Member member) throws Exception {
+		// 회원정보 수정
+		int count = mapper.modify(member);
+		// 회원권한 삭제
+		mapper.deleteAuth(member);
+
+		// 사용자가 선택한 권한 내용을 가져온다.
+		List<MemberAuth> authList = member.getAuthList();
+		for (int i = 0; i < authList.size(); i++) {
+			MemberAuth memberAuth = authList.get(i);
+			String auth = memberAuth.getAuth();
+
+			if (auth == null || auth.trim().length() == 0) {
+				continue;
+			}
+			// 변경된 회원권한 추가
+			memberAuth.setUserNo(member.getUserNo());
+			mapper.modifyAuth(memberAuth);
+		}
+		return count;
+	}
 }
