51078c541540ae2f926e000d686b0df2a633ffec
xsaz0826 <xsaz0826@gmail.com>
2026-02-25 11:42:51 +0900

    [추가] 회원관리 페이지 삭제 부분 화면 요청 처리를 위한 Controller 구현

--- src/main/java/com/project/controller/MemberController.java
+++ src/main/java/com/project/controller/MemberController.java
@@ -105,11 +105,24 @@ public class MemberController {
 	@PostMapping("/modify2")
 	public String modify(Member member, RedirectAttributes rttr) throws Exception {
 		int count = service.modify(member);
+
+		if (count != 0) {
+			rttr.addFlashAttribute("msg", "SUCCESS");
+		} else {
+			rttr.addFlashAttribute("msg", "FAIL");
+		}
+		return "redirect:/user/list";
+	}
+
+	// 삭제 처리
+	@PostMapping("/remove")
+	public String remove(Member member, RedirectAttributes rttr) throws Exception {
+		int count = service.remove(member);
 		
 		if (count != 0) {
 			rttr.addFlashAttribute("msg", "SUCCESS");
 		} else {
-			rttr.addFlashAttribute("msg", "F");
+			rttr.addFlashAttribute("msg", "FAIL");
 		}
 		return "redirect:/user/list";
 	}
